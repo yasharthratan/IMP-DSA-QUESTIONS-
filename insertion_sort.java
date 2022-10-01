f1bdab6052faff27dc2d2eb549eab1a73a8490a7
SwetaDas16 <95847009+SwetaDas16@users.noreply.github.com>
2022-10-01 23:38:05 +0530

    Create insertion_sort.java
    Please accept the pull request for hacktoberfest

---
+++ insertion_sort.java
@@ -0,0 +1,17 @@
+public class Insertion_sort {
+	public static void main(String[] args) {
+	int a[]	= {20,30,10,50};
+	for(int i = 0; i < a.length;i++)	{
+		for(int j = i+1; j< a.length;j++)	{
+			if(a[i]>a[j]) {
+				int temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+		}
+	}
+	for(int i = 0; i < a.length;i++)	{
+		System.out.print(a[i]+ " ");
+	}	
+}
+}
